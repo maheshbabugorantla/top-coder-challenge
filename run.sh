cf96f3ff5e3f673d6744bae79e58596e74455fe7
Mahesh Babu Gorantla <mahesh@projexel.vet>
2025-06-07 18:27:44 -0500

    Added run.sh file

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# This is a sample script that runs a Python command.
+# You can modify this script to run your own command.
+# For example, you can run a Python script like this:
+python3 -m src.main "$1" "$2" "$3"
+# Or, you can run a compiled C++ program like this:
+# ./my_program
